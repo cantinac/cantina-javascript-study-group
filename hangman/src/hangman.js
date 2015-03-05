e280375fb90d093e8a3e0a45cc6bd8206859bcfc
janetriley <jriley@janetriley.net>
2015-03-05 10:52:00 -0500

    Create hangman files

---
+++ hangman/src/hangman.js
@@ -0,0 +1,113 @@
+
+var word = undefined;
+var mistakes = 0;
+var MAX_MISTAKES = 6; //Two arms, two legs, one head, one body
+var lettersGuessed = {};
+
+/* =========== MAIN ============= */
+/*  There's always a function that coordinates the game, like a conductor. */
+
+//For a web page:
+//Setup stuff in document.onLoad():
+setTargetWord();
+
+function submitGuess(letter){
+	/*
+	checkAnswer();
+	
+	if( youWon()){
+		//Yay!
+		//show some feedback
+	} else if( youLost()){
+		//Sad trombone! 
+		//show some feedback
+	}
+	printPuzzle();
+	*/
+}
+
+/* For a command line Node program, it would run something like this to start
+function main(){
+	/*
+	//one-time setup:
+	intro();
+	setTargetWord();
+
+	//game play:
+	while( true){
+		//TODO: Read input from command line
+
+		if( youWon()){
+			//Yay!
+			break;
+
+		} else if( youLost()){
+			//Sad trombone!
+			break;
+		}
+		printPuzzle();
+	}*/
+}
+
+
+/*  ========== GAME FUNCTIONS ======== */
+
+
+/**
+* @brief Check if a letter is correct.  
+* If correct: mark the letter as correctly guessed 
+* If not: increment the mistake counter
+*/
+function checkAnswer(letter){
+}
+
+
+/**
+* @brief Print a welcome message and instructions.
+*/
+
+function intro(){
+
+}
+
+/**
+* @brief  Print the word.  
+* Print an underscore _ if the letter has not been guessed,
+* or the letter if it has been guessed.
+*/
+function printPuzzle(){
+
+}
+
+/**
+* @brief Pick a word for the user to guess.
+*
+*/
+function setTargetWord(newWord){
+	//To start, hardcode one word.
+	//For a fancier version, pick from a list of words.
+}
+
+
+/**
+* @brief  Does the word contain the letter?
+*/
+function wordContains(letter){
+
+}
+
+
+/**
+* @brief Check whether all letters have been guessed.
+*/
+function youWon(){
+
+}
+
+/**
+* @brief  Check whether the user has used up their mistakes. 
+*/
+function youLost(){
+
+}
+
---
+++ hangman/test/hangman_spec.js
@@ -0,0 +1,48 @@
+describe("Hangman  Game", function () {
+
+    beforeEach(function () {
+        setTargetWord = "abcde";
+        mistakes = 0;
+    });
+
+
+    describe("guesses", function () {
+        it("detects when word contains letter", function () {
+        });
+
+        it("remembers guesses", function () {
+        });
+
+        it("increments mistakes on bad guess", function () {
+        });
+
+    });
+
+
+    describe("winning and losing", function () {
+        it("detects a win", function () {
+        });
+
+        it("detects a lose", function () {
+        });
+    });
+
+    describe("displays puzzle", function () {
+        it("draws unguessed letters as dashes", function () {
+        });
+
+        it("reveals guessed letters", function () {
+        });
+    });
+
+
+    /* TEMPLATE
+     describe("", function(){
+        it("", function(){
+
+        });
+     });
+     */
+
+});
+
